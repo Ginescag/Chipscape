c83f494e337d0c0d413659510e232dbfc0f05d6a
Ginescag <ginescag_2@outlook.es>
2025-10-24 12:13:06 +0200

    physics are functional

--- src/assets/sc01_assets.asm
+++ src/assets/sc01_assets.asm
@@ -3,9 +3,9 @@ SECTION "scene 01 assets", ROM0
 sc01_entities::  ;;PYXp----
 sc01_entity1::   ;;SPRITE + PHYSICS
     DB 16, 32, $1A, %00000000   ;;SPRITE
-    DB 16, 32, 01, 00           ;;PHYSICS
+    DB 16, 32, 00, 00           ;;PHYSICS
 
     sc01_entity2::
     DB 16, 40, $1C, %00000000
-    DB 16, 40, 01, 01
+    DB 16, 40, 00, 00
   
